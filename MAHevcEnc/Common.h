0e5ec885fbc3cb2cb9704b747e8a8095e8745740
LAPTOP-VIEA5IEL\mayo2010 <mayo_mdhk2015@163.com>
2017-11-22 22:11:44 +0800

    add some defination of structure about ctu info

--- MAHevcEnc/Common.h
+++ MAHevcEnc/Common.h
@@ -11,7 +11,8 @@ typedef unsigned char Pel;
 
 #define MAX_CU_SIZE 64
 #define LOG2_MAX_CU_SIZE 6
-
+#define MAX_LENGTH 1024
+typedef short coeff_t;
 template<typename T>
 inline T x265_min(T a, T b) { return a < b ? a : b; }
 
@@ -29,7 +30,15 @@ enum HEVC_SLICE_TYPE
 	HEVC_INVALID_SLICE
 };
 
-#define MAX_LENGTH 1024
+enum CU_PART_MODE
+{
+	PART_2Nx2N = 0,
+	PART_2NxN,
+	PART_Nx2N,
+	PART_NxN,
+	MAX_PART_TYPE
+};
+
 typedef struct _GlobalParam
 {
 	_GlobalParam()
@@ -50,6 +59,47 @@ typedef struct _GlobalParam
 	bool	bEnSao;
 	bool	bEnTuSplit;
 }GlobalParam;
+
+typedef struct _TuInfo
+{
+
+}TuInfo;
+typedef struct _PuInfo
+{
+	uint8 uMergeIdx : 3;
+	uint8 bMergeFlag : 1;
+	uint8 bMvpL0Flag : 1;
+
+}PuInfo;
+
+typedef struct _TuCoeff8x8
+{
+	coeff_t tuCoeff[8][8];
+}TuCoeff8x8;
+
+typedef struct _CuInfo
+{
+	uint8 uCuIdxInCtu;
+	uint8 uIntraPredMode;
+	uint8 bSplitCuFlag : 1;
+	uint8 uLog2CuSize  : 6;//support 64x64 cu
+	uint8 bCuSkipFlag  : 1;
+
+	uint8 bCuTransByPass : 1;
+	uint8 bPredModeFlag : 1;
+	uint8 UCuPartMode : 2;
+	uint8 bPcmFlag : 1;
+
+
+	PuInfo puInfos[4];
+	TuInfo tuInfos[4];//only when PART_MODE != PART_2Nx2N,one cu can split into 4 tu in P slice
+}CuInfo;
+//store all the info need to be coded
+typedef struct _CtuInfo
+{
+	CuInfo cuInfos[64];
+	TuCoeff8x8 allTuCoeff[64];//
+}CtuInfo;
 
 class CYuvReader
 {
